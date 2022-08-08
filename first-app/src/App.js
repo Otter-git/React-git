ed070936c5243b97621342c1b138c3990a4f5309
alisa <alisa@Otter.localdomain>
2022-08-08 15:49:49 +0300

    homework points 1-3

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,11 +1,37 @@
 import { useState, useEffect } from "react";
-import Message from './Massage';
+import Message from './Message';
 import './App.css';
 
 function App() {
-  const text = 'and something from App';
+  const newText = 'and something from App';
+
+  const [messageList, setMessageList] = useState([]);
+
+  const updateMessageList = (event) => {
+    event.preventDefault();
+    setMessageList([...messageList, {"author": event.target.author.value, "text": event.target.text.value}]);
+  }
+
   return (
-    <Message addition={text} />
+    <>
+        <Message addition={newText} />
+        <div>
+          {messageList.map((item) => {
+            return(
+              <div key={item.author}>
+                {item.text}
+              </div>
+            )
+          })}
+        </div>
+        <form action="#" onSubmit={updateMessageList}>
+          <label>Введите имя</label>
+          <input type="text" name="author"/><br/>
+          <label>Введите текст сообщения</label>
+          <input type="text" name="text"/><br/>
+          <input type="submit"/>
+        </form>
+    </>
   );
 }
 
