f94b0496f1c0eb0f491738a6079237aec5bece1f
alisa <alisa@Otter.localdomain>
2022-08-17 14:10:42 +0300

    homework point 3

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,22 +1,13 @@
-import { useState, useEffect } from "react";
-import Container from "@mui/material/Container";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { teal } from '@mui/material/colors';
-import { deepOrange } from '@mui/material/colors';
 import Message from './Message';
 import './App.css';
-import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import Chats from "./pages/Chats";
 import Profile from "./pages/Profile";
 import CustomLink from "./components/CustomLink";
+import ChatsList from "./pages/ChatsList";
+import NotFound from './pages/NotFound';
 
-const theme = createTheme({
-  palette: {
-    primary: teal,
-    secondary: deepOrange,
-  }
-})
 
 function App() {
   const newText = 'and something from App';
@@ -31,7 +22,9 @@ function App() {
       <Routes>
         <Route path={'/'} element={<Home />} />
         <Route path={'/profile'} element={<Profile />} />
-        <Route path={'/chats'} element={<Chats />} />
+        <Route path={'/chats/:chatId'} element={<Chats />} />
+        <Route path={'/chats'} element={<ChatsList />} />
+        <Route path={'*'} element={<NotFound />} />
       </Routes>
     </>
   )
--- first-app/src/pages/Chats.js
+++ first-app/src/pages/Chats.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+import { useParams } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import SendIcon from "@mui/icons-material/Send";
 import Container from "@mui/material/Container";
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import CommentIcon from '@mui/icons-material/Comment';
-import IconButton from '@mui/material/IconButton';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { teal } from '@mui/material/colors';
 import { deepOrange } from '@mui/material/colors';
-
+import ListItem from '@mui/material/ListItem'; 
+import ListItemText from '@mui/material/ListItemText'; 
+import CommentIcon from '@mui/icons-material/Comment'; 
+import IconButton from '@mui/material/IconButton'; 
 
 const theme = createTheme({
   palette: {
@@ -24,18 +24,34 @@ const theme = createTheme({
 
 function Chats() {
     const [count, setCount] = useState(0);
-    const [messageList, setMessageList] = useState([]);
-    const [chatList, setChatList] = useState([]);
+    const [chats, setChats] = useState({
+        id1: {
+            name: "Chat1",
+            messages: [],
+            },
+        id2: {
+            name: "Chat2",
+            messages: [],
+            },
+    });
+    const { chatId } = useParams();
 
+    const showWrongParam = () => {
+        if (!(chatId in chats)) {
+            window.location.href = "/notfound";
+        }
+    }
+    
     const updateCount = () => {
         setCount(count + 1);
     }
 
-    const updateMessageList = (event) => {
+    const updateChatList = (event) => {
         event.preventDefault();
-        if (event.target.author.value != 'BOT') {
-            setMessageList([...messageList, {"id": count, "author": event.target.author.value, "text": event.target.text.value}]);
-            setChatList([...chatList, {"id": count, "name": event.target.author.value}]);
+        if (event.target.author.value != 'BOT' && event.target.author.value != '') {
+            setChats({...chats, [chatId]: {...chats[chatId],
+                messages: [...chats[chatId].messages, {"id": count, "author": event.target.author.value, "text": event.target.text.value}]
+            }});
             updateCount();
             event.target.text.focus();
         } else {
@@ -43,37 +59,40 @@ function Chats() {
         };
     };
 
+    useMemo(() => {
+        showWrongParam();
+    });
+
     useEffect(() => {
         setTimeout (() => {
-            if (messageList.length != 0 && messageList.slice(-1)[0].author != 'BOT'){
-            setMessageList([...messageList, {"id": count, "author": "BOT", "text": "Ваше обращение принято"}]);
+            if (chats[chatId].messages.length != 0 && chats[chatId].messages.slice(-1)[0].author != 'BOT'){
+                setChats({...chats, [chatId]: {...chats[chatId],
+                    messages: [...chats[chatId].messages, {"id": count, "author": "BOT", "text": "Ваше обращение принято"}]
+                }});
             updateCount();
             }
         }, 2000);
-    }, [messageList]);
+    }, [chats[chatId].messages]);
 
     return (
         <ThemeProvider theme={theme}>
             <Container sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                <Box component="form" noValidate onSubmit={updateMessageList} mt={6}>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'teal' }}>
+                        <ListItem 
+                            disableGutters 
+                            secondaryAction={<IconButton aria-label="comment"><CommentIcon /> 
+                            </IconButton> 
+                            }> 
+                            <ListItemText primary={`Вы находитесь в ${chats[chatId].name}`}/> 
+                        </ListItem>
+                </List>
+                <Box component="form" noValidate onSubmit={updateChatList} mt={6}>
                     <TextField label="Имя" name="author" sx={{ mb: 2 }}/>
                     <TextField label="Сообщение" name="text" autoFocus={true} sx={{ mb: 2 }}/>
                     <Button variant="outlined" type="submit" endIcon={<SendIcon />} sx={{ mb: 6 }}>Отправить</Button>
                 </Box>
-                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                    {chatList.map((item) => (
-                        <ListItem
-                            key={item.id}
-                            disableGutters
-                            secondaryAction={<IconButton aria-label="comment"><CommentIcon />
-                            </IconButton>
-                            }>
-                            <ListItemText primary={`Диалог с ${item.name}`}/>
-                        </ListItem>
-                    ))}
-                </List>
                 <div className="mesList">
-                    {messageList.map((item) => {
+                    {chats[chatId].messages.map((item) => {
                         return(
                             <div className="mesItem" key={item.id}>
                             <strong>{item.author}:</strong> {item.text}
